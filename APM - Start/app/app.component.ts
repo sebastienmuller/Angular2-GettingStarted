4b760244c5c5450adf6e0e80c081e40b832679c2
smuller <smuller-externe@sigma.fr>
2016-11-04 13:48:55 +0100

    add product-list.component

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'pm-app',
-    template: '\
-    <div><h1>{{pageTitle}}</h1>\
-        <div>My first component</div>\
-    </div>'
+    template: `
+    <div><h1>{{pageTitle}}</h1>
+        <pm-products></pm-products>
+    </div>`
 })
 export class AppComponent{
     pageTitle: string = 'Acme Product Management';
