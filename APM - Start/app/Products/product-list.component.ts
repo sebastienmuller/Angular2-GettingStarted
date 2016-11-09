f09122bd1e5dfe04b79f456f7760fffa878f2e30
Sébastien Muller <seb.muller@free.fr>
2016-11-09 22:13:06 +0100

    product-list : thead css + OnInit

--- APM - Start/app/Products/product-list.component.ts
+++ APM - Start/app/Products/product-list.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: 'app/products/product-list.component.html'
+    templateUrl: 'app/products/product-list.component.html',
+    styleUrls: ['app/products/product-list.component.css']
 })
-export class ProductListComponent{
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product list';
     imageWidth: number = 50;
     imageMargin: number = 2;
@@ -37,5 +38,9 @@ export class ProductListComponent{
 
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        console.log('In OnInit');
     }
 }
