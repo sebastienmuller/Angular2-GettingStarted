eadfa5ad27fe2054a0dc5f773907c92c426b5684
Sébastien Muller <seb.muller@free.fr>
2016-11-09 22:28:38 +0100

    product-list - filtre : add custom pipe

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { ProductListComponent }  from './products/product-list.component';
+import { ProductFilterPipe }  from './products/Product-Filter.Pipe';
 
 @NgModule({
   imports: [
@@ -11,7 +12,8 @@ import { ProductListComponent }  from './products/product-list.component';
     FormsModule ],
   declarations: [
     AppComponent,
-    ProductListComponent ],
+    ProductListComponent,
+    ProductFilterPipe ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
