504be788de8fd1486c0cd85f34a22eaa2dda0440
Sébastien Muller <seb.muller@free.fr>
2016-11-14 23:22:03 +0100

    product list : add Http and Observable

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { Http } from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import { ProductListComponent }  from './products/product-list.component';
@@ -10,7 +11,9 @@ import { StarComponent }  from './shared/star.component';
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule ],
+    FormsModule,
+    Http
+ ],
   declarations: [
     AppComponent,
     ProductListComponent,
