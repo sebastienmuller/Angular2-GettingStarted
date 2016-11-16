e2c06ee1eb60109857c715267efa21fc279fa3a6
seb.muller@free.fr <smuller-externe@sigma.fr>
2016-11-16 13:57:43 +0100

    Add product-detail.component

---
+++ APM - Start/app/Products/product-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { IProduct } from './product';
+
+@Component({
+    templateUrl: 'app/products/product-detail.component.html'
+})
+export class ProductDetailComponent {
+    pageTitle: string = 'Product detail';
+    product: IProduct;
+}
