d2c040fcbfe885aa2529cebf739f4d00ba945f72
Sébastien Muller <seb.muller@free.fr>
2016-11-16 22:44:54 +0100

    add routing and navigation

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { WelcomeComponent }  from './home/welcome.component';
+
 import { ProductListComponent }  from './products/product-list.component';
 import { ProductDetailComponent }  from './products/product-detail.component';
 import { ProductFilterPipe }  from './products/Product-Filter.Pipe';
@@ -14,7 +16,14 @@ import { StarComponent }  from './shared/star.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', component: WelcomeComponent, pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
  ],
   declarations: [
     AppComponent,
